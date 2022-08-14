b4841193a65364548f4dc5035f30e4867572f176
Alena <you@example.com>
2022-08-14 11:13:46 +0300

    Work_task_6

--- HomeWork_task6/Program.cs
+++ HomeWork_task6/Program.cs
@@ -7,7 +7,7 @@
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number % 2 == 1/2)
+if (number % 2 == 0)
 {
    Console.Write("Четное число,делится на 2");
 }
